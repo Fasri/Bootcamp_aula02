ef37bfdd6ac34f6ec6b334896fd59d6362fa4c12
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-01 08:15:13 -0300

    Adcionar exercicio 11

--- exercicio.py
+++ exercicio.py
@@ -50,7 +50,17 @@ import math
 # print(f"A temperatura em Fahrenheit é {fahrenheit}")
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-raio = float(input("Digite o raio do círculo para calcular a área: "))
-area = math.pi*raio**2
-print(f"A área do círculo é {area}")
+# raio = float(input("Digite o raio do círculo para calcular a área: "))
+# area = math.pi*raio**2
+# print(f"A área do círculo é {area}")
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+palavra = input("Digite uma String: ")
+maiuscula = palavra.upper()
+print(maiuscula)
+
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
