89e5a3203d085a2c1400e26f1d912bb4af2fc21e
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-01 07:28:06 -0300

    Adcionar exercicio 04

--- exercicio.py
+++ exercicio.py
@@ -7,11 +7,16 @@
 # numero = float(input("Insira um número para saber o resto da divisão por 5: "))
 # resto = numero//5
 # print(f"O resto da divisão por 5 é {resto}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-primeiro_numero = int(input("Digite o primeiro número: "))
-segundo_numero = int(input("Digite o segundo número: "))
-resultado = primeiro_numero*segundo_numero
-print(f"A multiplicação é {resultado}")
+# primeiro_numero = int(input("Digite o primeiro número: "))
+# segundo_numero = int(input("Digite o segundo número: "))
+# resultado = primeiro_numero*segundo_numero
+# print(f"A multiplicação é {resultado}")
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+primeiro_numero = int(input("Digite o primeiro número: "))
+segundo_numero = int(input("Digite o segundo número: "))
+resultado = primeiro_numero/segundo_numero
+print(f"A divisão é {resultado}")
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
