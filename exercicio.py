5697f17811c00a97f8ede27cdb4366ddc8bf2d33
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-01 20:34:36 -0300

    Adciconar exercici 20

--- exercicio.py
+++ exercicio.py
@@ -101,10 +101,15 @@ import math
 # print("Resultado do NOT é: ", bool_not)
 
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
-numero1 = 4
-numero2 = 4
-resultado = (numero1 == numero2)
-print("O resultado sa igualdade é : ", resultado)
+# numero1 = 4
+# numero2 = 4
+# resultado = (numero1 == numero2)
+# print("O resultado sa igualdade é : ", resultado)
 
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+resultado = (numero1 != numero2)
+print("O resultado da diferença é: ", resultado)
+
 
