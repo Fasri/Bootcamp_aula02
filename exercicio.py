a1d2887f28a03e6a2abdd6304cf9897086dae463
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-01 06:22:26 -0300

    adcionar execicio 01

---
+++ exercicio.py
@@ -0,0 +1,10 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# primeiro_numero = int(input("Digite o primeiro número: "))
+# segundo_numero = int(input("Digite o segundo número: "))
+# print(f"A soma é {primeiro_numero+segundo_numero}")
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
