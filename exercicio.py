fa241d7e494de237226dcdfbb6496c815df6c2fa
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-01 08:20:42 -0300

    Adcionar exercicio 12

--- exercicio.py
+++ exercicio.py
@@ -55,11 +55,15 @@ import math
 # print(f"A área do círculo é {area}")
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-palavra = input("Digite uma String: ")
-maiuscula = palavra.upper()
-print(maiuscula)
+# palavra = input("Digite uma String: ")
+# maiuscula = palavra.upper()
+# print(maiuscula)
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+nome = input("Digite um nome: ")
+nome_modificado = nome.lower()
+print(nome_modificado)
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
