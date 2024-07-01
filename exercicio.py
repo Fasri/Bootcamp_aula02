f970bfac97d514c3af76cca7afc67f893b009547
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-01 06:30:00 -0300

    Adcionar execício 02

--- exercicio.py
+++ exercicio.py
@@ -4,7 +4,9 @@
 # print(f"A soma é {primeiro_numero+segundo_numero}")
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-
+numero = float(input("Insira um número para saber o resto da divisão por 5: "))
+resto = numero//5
+print(f"O resto da divisão por 5 é {resto}")
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
