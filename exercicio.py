06558e51733a0dbd0052ac113da6fe7a3c6d95f8
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-09 06:59:27 -0300

    Adicionar exercicio 24

--- exercicio.py
+++ exercicio.py
@@ -138,31 +138,45 @@ import math
 
 # Exercício 23: Calculadora Simples
 # Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. Use try-except para lidar com divisões por zero e entradas não numéricas. Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. Imprima o resultado ou uma mensagem de erro apropriada.
-try:
-    num1 = float(input("Digite o primeiro numero: "))
-    num2 = float(input("Digite o segundo numero: "))
-    ope = input("Qual operação vai ser realizada? Digite +, -, x ou /")
-    if ope == "+":
-        resultado = num1 + num2
-        print(f"A soma é {resultado}")
-    elif ope == "-":
-        resultado = num1 - num2
-        print(f"A subtração é {resultado}")
-    elif ope == "x":
-        resultado = num1 * num2
-        print(f"A multiplicação é {resultado}")
-    elif ope == "/":
-        resultado = num1 / num2 and num2 != 0
-        print(f"A divisão é {resultado}")
-    else:
-        print("Operador invalido ou divisão por zero")
+# try:
+#     num1 = float(input("Digite o primeiro numero: "))
+#     num2 = float(input("Digite o segundo numero: "))
+#     ope = input("Qual operação vai ser realizada? Digite +, -, x ou /")
+#     if ope == "+":
+#         resultado = num1 + num2
+#         print(f"A soma é {resultado}")
+#     elif ope == "-":
+#         resultado = num1 - num2
+#         print(f"A subtração é {resultado}")
+#     elif ope == "x":
+#         resultado = num1 * num2
+#         print(f"A multiplicação é {resultado}")
+#     elif ope == "/":
+#         resultado = num1 / num2 and num2 != 0
+#         print(f"A divisão é {resultado}")
+#     else:
+#         print("Operador invalido ou divisão por zero")
 
-except ValueError:
-    print("Apenas números!")
+# except ValueError:
+#     print("Apenas números!")
 
 # Exercício 24: Classificador de Números
 # Escreva um programa que solicite ao usuário para digitar um número. Utilize try-except para assegurar que a entrada seja numérica e utilize if-elif-else para classificar o número como "positivo", "negativo" ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".
-
+try:
+    num = float(input("Digite um número: "))
+    if num > 0:
+        print(f"O número {num} é positivo.")
+    elif num < 0:
+        print(f"O número {num} é negativo.")
+    else:
+        print(f"O número {num} é zero.")
+    if num %2 == 0:
+        print(f"O número {num} é par.")
+    else:
+        print(f"O número {num} é ímpar.")
+except ValueError:
+    print("Digite um número!")
+    
 # Exercício 25: Conversão de Tipo com Validação
 # Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de entrada em uma lista de números inteiros. Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
 
