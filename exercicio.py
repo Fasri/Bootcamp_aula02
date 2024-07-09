5b27329eef7f9c008bc73d52575c43a9bcac4504
Fasri <106215404+Fasri@users.noreply.github.com>
2024-07-09 05:55:04 -0300

    Adciconar exercicio 22

--- exercicio.py
+++ exercicio.py
@@ -114,17 +114,28 @@ import math
 
 # Exercício 21: Conversor de Temperatura
 # Escreva um programa que converta a temperatura de Celsius para Fahrenheit. O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, garantir que a entrada seja numérica, tratando qualquer ValueError. Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
-try:
-    celsius = float(input("Digite a temperatura em celsius: "))
-    fahrenheit = (celsius * 9/5) + 32
-    print(f"A temperatura em Fahrenheit é {fahrenheit}")
-except ValueError:
-    print("Digite um valor válido para a temperatura em Celcius")
+# try:
+#     celsius = float(input("Digite a temperatura em celsius: "))
+#     fahrenheit = (celsius * 9/5) + 32
+#     print(f"A temperatura em Fahrenheit é {fahrenheit}")
+# except ValueError:
+#     print("Digite um valor válido para a temperatura em Celcius")
 
 
 # Exercício 22: Verificador de Palíndromo
 # Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
 
+palavra = input("Digite uma palavra ou frase: ")
+if isinstance(palavra, str):
+    palavra = palavra.replace(" ", "").lower()
+    palavra_invertida = palavra[::-1]
+    if palavra == palavra_invertida:
+        print(f"A palavra {palavra} é palíndromo")
+    else:
+        print(f"A palavra {palavra} não é palíndromo, a palavra invertida fica {palavra_invertida}")
+else:
+    print("Entrada invalida, Por favor, digite uma palavra ou frase")
+    
 # Exercício 23: Calculadora Simples
 # Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. Use try-except para lidar com divisões por zero e entradas não numéricas. Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. Imprima o resultado ou uma mensagem de erro apropriada.
 
